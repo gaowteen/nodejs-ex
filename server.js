c51827419e04c1c71e268f728a4e2684ccdb097e
Bird Boonnoon <gaowteen@hotmail.com>
2018-02-10 10:46:35 +0700

    Some commit message

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'))
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
     mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
     mongoURLLabel = "";
 
@@ -73,7 +73,8 @@ app.get('/', function (req, res) {
       res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
     });
   } else {
-    res.render('index.html', { pageCountMessage : null});
+    res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
+    // res.render('index.html', { pageCountMessage : null});
   }
 });
 
